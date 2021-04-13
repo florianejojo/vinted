259988fa28803458e9e675983a420b3d306841c8
Floriane Lefebvre <flolefeb@student.42.fr>
2021-04-13 15:37:58 +0200

    ok route 1 without exeptions

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
-const formidable = require("formidable");
+const formidable = require("express-formidable");
 const mongoose = require("mongoose");
 
-const app = express();
+mongoose.connect("mongodb://localhost/vinted", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
 
+const app = express();
 app.use(formidable());
+
+const userRoutes = require("./routes/user");
+app.use(userRoutes);
+
+app.listen(3000, () => {
+    console.log("Server started");
+});
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
+const uid2 = require("uid2");
+const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
 
-router.post("/user/signup", (req, res) => {
-    res.json("/user/signup");
+router.post("/user/signup", async (req, res) => {
+    try {
+        const { email, username, phone, password } = req.fields;
+        const salt = uid2(16);
+        const hash = SHA256(salt + password).toString(encBase64);
+        const token = uid2(64);
+        const newUser = new User({
+            email: email,
+            account: {
+                username: username,
+                phone: phone,
+                avatar: {},
+            },
+            token: token,
+            hash: hash,
+            salt: salt,
+        });
+
+        await newUser.save();
+        res.status(200).json("User added ");
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
 
 router.post("/user/login", (req, res) => {
@@ -13,6 +38,4 @@ router.all("*", (req, res) => {
     res.json("Bad getaway");
 });
 
-router.listen(3000, () => {
-    res.json("server started");
-});
+module.exports = router;
